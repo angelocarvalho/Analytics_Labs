dad31796aa655398446ebad473726bb470882730
angelocarvalho <carvaa@amazon.com>
2021-03-05 10:12:51 -0300

    removing checkpoint folder

--- msk_emr_spark_streaming/.ipynb_checkpoints/2_create_topic-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-wget https://archive.apache.org/dist/kafka/2.2.1/kafka_2.12-2.2.1.tgz
-tar -xzf kafka_2.12-2.2.1.tgz
-msk_host="endpoint:port,endpoint:port,endpoint:port"
-kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $msk_host --replication-factor 2 --partitions 1 --topic AWSKafkaTutorialTopic
